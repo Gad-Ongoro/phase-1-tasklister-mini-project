cbf819289894a30cb1020b336f9e3c4e8ac09d75
Gad-Ongoro <gadongoro1@gmail.com>
2023-10-01 21:24:50 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let li;
+
+  let task = document.getElementById("new-task-description");
+
+  let tasks = (document.querySelector("#tasks"))
+
+  document.querySelector("form").addEventListener("submit", function(e){
+    e.preventDefault();
+    addLi(task.value); 
+  });
+
+  function addLi(todo) {
+    li = document.createElement("li");
+    li.innerHTML = todo;
+    tasks.appendChild(li);
+
+    delTasks();
+  };
+
+  function delTasks() {
+    let x = document.createElement("button");
+    x.innerHTML = "X";
+    li.appendChild(x);
+
+    x.addEventListener("click", () => {
+      x.parentNode.remove();
+    })
+  };
 });
